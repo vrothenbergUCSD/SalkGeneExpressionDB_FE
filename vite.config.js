db70a62cd6153fdb8da41a2910441fa75e06a25d
vrothenbergUCSD <vrothenberg@ucsd.edu>
2022-07-26 14:33:11 -0700

    npm run build vite.config.js outDir public

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "public",
+  },
   server: {
     proxy: {
       "/api": {
@@ -20,9 +23,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outdir: "/public"
-  }
+
 
 })
 
