0d6efa84a8066b02fe8387955beabe182dba6770
vrothenbergUCSD <vrothenberg@ucsd.edu>
2022-07-26 10:43:21 -0700

    New branch

--- src/services/CountryService.js
+++
@@ -1,9 +0,0 @@
-export default class CountryService {
-
-  async getCountries() {
-    console.log("getCountries")
-
-    return fetch('data/countries.json').then(res => res.json())
-        .then(d => d.data);
-  }
-}
