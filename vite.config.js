9050998cdfac48424e0ea2235f91d2d5cccbb0ee
vrothenbergUCSD <vrothenberg@ucsd.edu>
2022-07-27 11:32:12 -0700

    Testing API url change between prod vs dev

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const defaultConfig = {
   build: {
     outDir: "public",
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://geneexpressiondbbe-w5oumbwxcq-uw.a.run.app",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
   },
   plugins: [vue()],
   resolve: {
@@ -23,6 +12,41 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    const isDev = mode === 'development'
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          '/api': {
+            target: isDev ? 'http://127.0.0.1:8000' : 'https://geneexpressiondbbe-w5oumbwxcq-uw.a.run.app',
+            changeOrigin: isDev,
+            secure: !isDev,
+            rewrite: (path) => path.replace(/^\/api/, ""),
+          }
+        }
+      }
+    }
+  } else {
+    return defaultConfig
+  }
+
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       // target: "https://geneexpressiondbbe-w5oumbwxcq-uw.a.run.app",
+  //       target: "http://127.0.0.1:8000",
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
+
 
 
 })
